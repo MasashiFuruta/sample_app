35ba8fee1c8f2f3191cbbc7f9bf6db2893f7041c
MasashiFuruta <33737185+MasashiFuruta@users.noreply.github.com>
2018-08-25 18:19:35 +0900

    Finish static pages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   # クロスサイトスクリプティングの防止
-  protect_from_forgery with::exception
+  protect_from_forgery with: :exception
 
   def hello
     render html: "hello, world!"
