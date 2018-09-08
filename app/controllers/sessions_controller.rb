0b142747b37a39e2a9c84dd4c7c3f679afa9154c
MasashiFuruta <33737185+MasashiFuruta@users.noreply.github.com>
2018-09-08 17:59:28 +0900

    Finish user edit, update, index, and destroy actions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -11,7 +11,8 @@ class SessionsController < ApplicationController
       # チェックボックスにチェックがあるかどうかで分岐させる
       params[:session][:remember_me] == '1'? remember(@user):forget(@user)
       # redirect_to user_url(@user)と一緒
-      redirect_to @user
+      # redirect_to @user
+      redirect_back_or @user
     else
       flash.now[:danger] = 'Invalid email/password combination'
       render 'new'
